c7a760239b658527d9a9642f732b17dea2ce3167
kar <karnikaa17@gmail.com>
2019-12-09 17:46:03 +0530

    initial commit login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header.component';
+import { BannerComponent } from './banner.component';
+import { FooterComponent } from './footer.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,HeaderComponent,BannerComponent,FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    HttpClientModule
+   
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
